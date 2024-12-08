6bb327b8e9c6be69c22e30df86fd0d27f80d605c
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-12-09 00:00:54 +0200

    feat: operations in c++

---
+++ Cpp-Basics/operations.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << 20*5 << endl;
+    cout << 20/5 << endl;
+    cout << 20/6 << endl;
+    cout << 20-5 << endl;
+    cout << 20+5 << endl;
+    cout << 20%5 << endl;
+
+// compare 
+    cout << (20<5) << endl;
+    //logical operators
+    int x= 5 , y= 10;
+    cout << (x==y) << endl;
+    cout << (x!=y) << endl;
+    cout << (x !=6 && y!=10 ) << endl;
+    cout << (x !=6 || y!=10 ) << endl;
+    return 0;
+}
