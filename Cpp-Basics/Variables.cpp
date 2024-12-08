8511bae0bd2918a28f6717dc85bba45291a7f4d1
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-12-08 23:15:51 +0200

    feat: add variables in c++

---
+++ Cpp-Basics/Variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x = 5 , y = 10;
+    float z= 1.5;
+    double d = 2.5;
+    bool b= true; // true = 1 , false = 0  any number  means true 
+    char c = 'a';
+    string s = "Hello";
+
+    cout << x << " " << y<< endl;
+    cout << s;
+
+    return 0;
+}
