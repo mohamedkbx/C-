bf24ebe29c1a64b5486a40d10bc257e5dcdca8f3
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-12-08 22:08:37 +0200

    feat: add outputs and comments in c++ and multi line comments - searching about iosteam library

---
+++ Cpp-Basics/outputs-and-comments.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+ int main (){
+
+    cout << "Moo\t" <<  "kaled\n" ;
+    cout << "Moo" <<"\n"<<  "kaled\n" ;
+    cout << "Moo\t" <<endl <<  "kaled" ;
+// this is comment 
+
+/*
+
+this is multi line comment
+
+*/ 
+    return 0;
+  
+ }
